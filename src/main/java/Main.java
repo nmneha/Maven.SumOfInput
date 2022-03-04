b673af6e0f8f3fdc1f1210d2a9e774f13ab7de0e
Nusera <nusera@zipcoders-MBP.fios-router.home>
2022-03-03 20:17:46 -0500

    create system to add the sum of the integer the user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        int total = 1;
+        System.out.println("Enter a number");
+        n = scanner.nextInt();
+        for (int i = 1; i < n; i++) {
+            total = total + (i+1);
+        }
+        System.out.print("The total sum of all numbers added from 1 to " + n + " is: " + total);
     }
+
+
+
+
 }
